98e60fac54c78bb9e3837e01bfe8ecd29d8414c1
Legolas <geran.vanatta@gmail.com>
2022-06-01 21:27:07 -0500

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <section>
+      <h1 id="contact-me" class="contact">Contact Me</h1>
+      <nav>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/geran-vanatta/">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://github.com/geranv1020">Github</a>
+                </li>
+                <li>
+                    <a href="mailto:Geran.Vanatta@gmail.com">Geran.vanatta@gmail.com</a>
+                </li>
+                <li>
+                    <a href="tel:+6154764617">615.476.4617</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+  );
+}
+
+export default Footer;
